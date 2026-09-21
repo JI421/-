096cb3596abe7f563a414e734043b4c756bc5699
JI421 <u310602@tcivs.tc.edu.tw>
2026-09-21 19:51:22 +0800

    Update YOLO4.py

--- YOLO4.py
+++ YOLO4.py
@@ -55,7 +55,7 @@ start_button = st.button("🚀 開啟攝影機並開始偵測", type="primary", 
 
 # ===== 4. 主要邏輯 =====
 if start_button:
-    cap = cv2.VideoCapture(2)
+    cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 
     if not cap.isOpened():
         status_box.error("❌ 無法開啟攝影機，請確認設備連接。")
